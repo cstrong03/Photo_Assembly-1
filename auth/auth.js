2142913c140f723e0e05f9536e59646cdd373287
Stephen Chen <rursteve2@yahoo.com>
2019-05-14 15:03:19 -0400

    started auth, no encrypt yet

--- auth/auth.js
+++ auth/auth.js
@@ -6,105 +6,100 @@ const jwt = require('jsonwebtoken')
 const JWTStrategy = require('passport-jwt').Strategy
 const ExtractJWT = require('passport-jwt').ExtractJwt
 
-const SECRET = "mojo jojo token super secret!" //do not do this in real application, use ENV variable instead
+const SECRET = 'super long string' // use an ENV variable & more secure string in a real app
 
 const jwtSign = (payload) => {
-    return jwt.sign(payload, SECRET)
+  return jwt.sign(payload, SECRET)
 }
 
-passport.use(new JWTStrategy({
-    secretOrKey: SECRET,
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken()
-  }, async(token, done) => {
-    console.log(`YOYOYO`)
-    try {
-        console.log(" *** decoded token ***", token)
-      const user = await User.findByPk(token.id);
-      console.log(`user from jwt Token: ${user}`)
-
-      if (user) {
-        done(null, user)
-      } else {
-        done(null, false)
-      }
-    } catch(error) {
-      done(error)
-    }
-  }))
-
 passport.use('signup', new LocalStrategy({
-    nameField: 'name',
-    usernameField: 'email',
-    passwordField: 'password'
-}, async (  email, password, done) => {
-    try {
-        console.log("in signup try!")
-        const user = await User.create({ email, password })
-        if (!user) {
-            return done(null, false, { message: 'Unable to sign up user'})
-        }
+  usernameField: 'username',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, username, password, done) => {
+  try {
+    const { body: { name } } = req
 
-        done(null, user)
-    } catch (e) {
-        console.log(e)
-        done(e)
+    const user = await User.create({
+      name: name,
+      username: username,
+      password: password
+    })
+
+    if (!user) {
+      return done(null, false, { message: 'Unable to sign up user'})
     }
+
+    done(null, user, { message: 'User created successfully'})
+  } catch (error) {
+    done(error)
+  }
+}))
+
+passport.use(new JWTStrategy({
+  secretOrKey: SECRET,
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken()
+}, async(token, done) => {
+  try {
+    const user = await User.findByPk(token.id)
+
+    if (user) {
+      done(null, user)
+    } else {
+      done(null, false)
+    }
+  } catch (error) {
+    done(error)
+  }
 }))
 
 passport.use('login', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-  }, async (email, password, done) => {
-    try {
-      // find user by their email
-      const user = await User.findOne({ where: { email: email }})
-      console.log(user.email)
+  usernameField: 'username',
+  passwordField: 'password'
+}, async (username, password, done) => {
+  try {
+    // find user by their email
+    const user = await User.findOne({ where: { username: username }})
 
-      console.log(`*** user: ${user} ***`)
-
-      if (!user) {
-        return done(null, false, { message: 'User not found'})
-      }
-
-      // compare passwords
-      const validate = await bcrypt.compare(password, user.password);
-      console.log(`*** validate: ${validate} ***`)
-
-      if (!validate) {
-        return done(null, false, { message: 'Wrong password'})
-      }
-
-      // login was a success, return the user object
-      return done(null, user, { message: 'Logged in successfully'})
-
-    } catch(error) {
-      return done(error)
+    if (!user) {
+      return done(null, false, { message: 'User not found'})
     }
-  }))
 
-  /**
+    // compare passwords
+    const validate = await bcrypt.compare(password, username.password)
+
+    if (!validate) {
+      return done(null, false, { message: 'Wrong password'})
+    }
+
+    // login was a success, return the user object
+    return done(null, user, { message: 'Logged in successfully'})
+  } catch (error) {
+    return done(error, false, { message: 'User not found'})
+  }
+}))
+
+/**
  * middleware for checking authorization with jwt
+ * source: https://github.com/mikenicholson/passport-jwt/issues/153#issuecomment-419627512
  */
 const authorized = (request, response, next) => {
-    passport.authenticate('jwt', { session: false, }, async (error, token) => {
-      if (error || !token) {
-        // response.status(401).json({ message: 'Unauthorized' });
-        let err = new Error('Hello, pew pew pew');
-        err.status = 401;
-        next(err)
-      }
-      try {
-        const user = await User.findOne({ where: { id: token.id }});
-        request.user = user;
-      } catch (error) {
-        next(error);
-      }
-      next();
-    })(request, response, next);
-  }
-
-  module.exports = {
-      passport,
-      jwtSign,
-      authorized
+  passport.authenticate('jwt', { session: false }, async (error, user) => {
+    if (error || !user) {
+      // response.status(401).json({ message: 'Unauthorized' });
+      let err = new Error('No bueno, no access allowed')
+      err.status = 401
+      return next(err)
     }
+
+    request.user = user
+    return next()
+  })(request, response, next)
+}
+
+module.exports = {
+  // export enhanced passport object (with strategy)
+  passport,
+  jwtSign,
+  authorized
+}
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,55 +1,74 @@
-const express = require('express');
-const authRouter = express.Router();
-const { passport, jwtSign } = require("../auth/auth")
-const { Users } = require('../models.js')
+const express = require('express')
+const authRouter = express.Router()
+const { passport, jwtSign } = require('../auth/auth.js')
 const bcrypt = require('bcrypt')
+const { User } = require('../models.js')
+
 
 
 authRouter.post('/signup', async(req, res, next) => {
-    passport.authenticate('signup', async(err, user, info) => {
-        try {
-            // const hasheduser = (user) => {bcrypt.hash(user.password, 12)};
-            // hasheduser(user)
-            if (!user || err) {
-                let err = new Error("Unable to create account.")
-                err.status = 400
-                return next(err)
-            }
-            console.log('user from /signup:', username)
-            return res.json({username, message: "User successfully created!"})
-        } catch (error) {
-            return next(error)
-        }
-    })(req, res, next)
+  passport.authenticate('signup', async(err, user, info) => {
+    try {
+      if (err) {
+          const hashedUser = (user) => {bcrypt.hash(user.password, 12)}
+          hashedUser(User)
+        let error = new Error(err.message || info.message)
+        error.status = 400
+        return next(error)
+      }
+
+      if (!user) {
+        return res.status(401).json({message: info.message})
+      }
+      const { username, id } = user
+      const payload = { username, id }
+
+      const token = jwtSign(payload)
+      return res.json({user: user, token: token, message: info.message})
+    } catch (error) {
+      return next(error)
+    }
+  })(req, res, next)
 })
 
-authRouter.post('/login', async (req, res, next) => {
-    // res.status(200).json({ message: "So far so good" })
+// matches '/auth/login' route
+authRouter.post('/login', (req, res, next) => {
+//   res.status(200).json({message: "So far so good!"})
+  passport.authenticate('login', async(err, user, info) => {
+    try {
+      let error
+      if (err) {
+        error = new Error(err.message)
+        error.status = 500
 
-    passport.authenticate('login', async(err, user, info) => {
-        try {
-            console.log('errrr:', err)
-          if (err || !user) {
-            const error = new Error('An Error Occurred')
-            return next(error)
-          }
+        return next(error)
+      }
 
-          req.login(user, { session : false }, async (error) => {
-            if ( error ) return next(error)
+      if (!user) {
+        error = new Error(info.message)
+        error.status = 400
+        return next(error)
+      }
 
-            const { email, id } = user
-            const payload = { username, id }
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error)
 
-            const token = jwtSign(payload)
-            // return the user object
-            return res.json({ username, token })
-          })
-
-        } catch(error) {
-          return next(error)
-
+        if (!user) {
+          let err = new Error(info.message)
+          err.status = 400
+          return next(err)
         }
-      })(req, res, next)
+
+        const { username, id } = user
+        const payload = { username, id }
+
+        const token = jwtSign(payload)
+        return res.json({ user, token })
+      })
+    } catch (error) {
+      return next(error)
+    }
+  })(req, res, next)
 })
 
 module.exports = authRouter
