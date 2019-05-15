feaaccd0b02c6fdf6acdc88dd587d256c1d00d76
Charisse Strong <msstrong92@gmail.com>
2019-05-15 10:05:39 -0400

    services folder started

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,24 +2,36 @@ import axios from 'axios';
 const URL = 'http://localhost:4567';
 
 const api = axios.create({
-    baseURL: `${URL}/`
+    baseURL: `${URL}/post`
 })
 
-export const getUser = async () => {
-    try {
-        const resp = await api.get('/user')
-        return resp.data;
-    } catch (e) {
-        console.log(e);
-    }
+export const fetchPost = async()=>{
+  try {
+    const resp = await api.get('/')
+    console.log(resp);
+    return resp.data.posts
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-export const createAPost = async (data) => {
-    try {
-        const resp = await api.post('/post', data)
-        console.log(resp)
-        return resp.data
-    } catch (e) {
-        console.log(e)
-    }
-}
+
+
+// export const getUser = async () => {
+//     try {
+//         const resp = await api.get('/user')
+//         return resp.data;
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+//
+// export const createAPost = async (data) => {
+//     try {
+//         const resp = await api.post('/post', data)
+//         console.log(resp)
+//         return resp.data
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
