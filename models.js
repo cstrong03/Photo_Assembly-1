d2aa9292b7bb878271560ea40ae3e8e7bb549548
Stephen Chen <rursteve2@yahoo.com>
2019-05-16 17:59:18 -0400

    fixed seeding data hash

--- models.js
+++ models.js
@@ -67,6 +67,10 @@ Comment.belongsTo(Post)
 // Like.belongsTo(Post)
 Post.belongsTo(User)
 
+User.beforeCreate( async (user, options) => {
+    const hashedPassword = await bcrypt.hash(user.password, 12) 
+      user.password = hashedPassword;
+    });
 
 module.exports = {
   db,
